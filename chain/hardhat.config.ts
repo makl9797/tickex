f642b21aa2e6b0b9b81c2a440a7ea77168b498ce
Mats Klein <mats.klein@naymspace.de>
2023-12-27 14:48:50 +0100

    change chainId

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -1,28 +1,13 @@
-// import { HardhatUserConfig } from "hardhat/config";
-// import "@nomicfoundation/hardhat-toolbox-viem";
-
-// const POLYGON_MUMBAI_RPC_URL = "https://rpc-mumbai.maticvigil.com";
-
-// const config: HardhatUserConfig = {
-//   solidity: "0.8.19",
-//   networks: {
-//     hardhat: {
-//       chainId: 80001,
-//       forking: {
-//         url: POLYGON_MUMBAI_RPC_URL
-//       }
-//     }
-//   }
-// };
-
-// export default config;
-
-
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }
+  }
 };
 
 export default config;
