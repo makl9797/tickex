9b9f5ce108977d0365fd00da64c9b421a9a0b46a
Mats Klein <mats.klein@naymspace.de>
2023-12-31 17:07:52 +0100

    remove viem

--- chain/hardhat.config.ts
+++ chain/hardhat.config.ts
@@ -1,13 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
-  networks: {
-    hardhat: {
-      chainId: 1337
-    }
-  }
 };
 
 export default config;
