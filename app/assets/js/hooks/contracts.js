adb792129312ee6852e51e9caa359d0c0d6bf1fd
Mats Klein <mats.klein@naymspace.de>
2024-01-03 00:49:01 +0100

    add contracts hooks

--- app/assets/js/hooks/contracts.js
+++ app/assets/js/hooks/contracts.js
@@ -1,31 +1,26 @@
 import { ethers } from "ethers";
+import EventManagementAbi from "../abis/EventManagement.json";
+import TicketManagementAbi from "../abis/TicketManagement.json";
 
-// Import ABIs
-import EventManagementAbi from "./abis/EventManagement.json";
-import TicketManagementAbi from "./abis/TicketManagement.json";
-
-// Define contract addresses (replace with actual addresses)
-const EVENT_MANAGEMENT_ADDRESS = "0xYourEventManagementContractAddress";
-const TICKET_MANAGEMENT_ADDRESS = "0xYourTicketManagementContractAddress";
+const EVENT_MANAGEMENT_ADDRESS = "0x91cA0DaA14c1aae433de956d4E10cd60d36E995a";
+const TICKET_MANAGEMENT_ADDRESS = "0x2814027dAF6B1770ABEbAA4E598A0671285Ab8e6";
 
 const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
 
 export const Contracts = {
     mounted() {
         const signer = web3Provider.getSigner();
-
-        // Create contract instances
         const eventManagement = new ethers.Contract(EVENT_MANAGEMENT_ADDRESS, EventManagementAbi, signer);
         const ticketManagement = new ethers.Contract(TICKET_MANAGEMENT_ADDRESS, TicketManagementAbi, signer);
 
         window.addEventListener(`phx:create-event`, async (e) => {
             const { ticketPrice, ticketsAvailable } = e.detail;
-            await eventManagement.createEvent(ethers.utils.parseUnits(ticketPrice, "ether"), ticketsAvailable);
+            await eventManagement.createEvent(ethers.utils.parseUnits(ticketPrice, "matic"), ticketsAvailable);
         });
 
         window.addEventListener(`phx:buy-ticket`, async (e) => {
             const { eventId, ticketPrice } = e.detail;
-            await ticketManagement.buyTicket(eventId, { value: ethers.utils.parseUnits(ticketPrice, "ether") });
+            await ticketManagement.buyTicket(eventId, { value: ethers.utils.parseUnits(ticketPrice, "matic") });
         });
 
         window.addEventListener(`phx:redeem-ticket`, async (e) => {
