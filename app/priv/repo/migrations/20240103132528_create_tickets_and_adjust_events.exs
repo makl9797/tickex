dc3ffcee6974383857329763fb3d9535c7460a2f
Mats Klein <mats.klein@naymspace.de>
2024-01-03 18:29:09 +0100

    fix migration

--- app/priv/repo/migrations/20240103132528_create_tickets_and_adjust_events.exs
+++ app/priv/repo/migrations/20240103132528_create_tickets_and_adjust_events.exs
@@ -14,6 +14,7 @@ defmodule Tickex.Repo.Migrations.CreateTicketsAndAdjustEvents do
     create table(:tickets, primary_key: false) do
       add(:id, :binary_id, primary_key: true)
       add(:purchase_date, :utc_datetime)
+      add(:purchase_price, :float)
       add(:redeemed, :boolean, default: false, null: false)
       add(:event_id, references(:events, on_delete: :nothing, type: :binary_id))
       add(:buyer_id, references(:users, on_delete: :nothing, type: :binary_id))
