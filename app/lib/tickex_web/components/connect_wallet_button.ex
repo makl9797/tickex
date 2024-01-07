ad585537f34fc6252c76ae4872547d6731f96f53
Mats Klein <mats.klein@naymspace.de>
2024-01-07 19:01:08 +0100

    add profile dropdown

--- app/lib/tickex_web/components/connect_wallet_button.ex
+++ app/lib/tickex_web/components/connect_wallet_button.ex
@@ -50,11 +50,69 @@ defmodule TickexWeb.Components.ConnectWalletButton do
           <div class="bg-blue-100 text-blue-800 text-sm font-semibold px-4 py-2 rounded-lg">
             <%= short_wallet_address(@current_wallet_address) %>
           </div>
-          <.form for={%{}} action={~p"/logout"} method="delete" as={:user} phx-trigger-action={@logout}>
-            <button type="submit" class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
-              Logout
-            </button>
-          </.form>
+          <div class="relative ml-3">
+            <div>
+              <button
+                phx-click={toggle_dropdown()}
+                phx-click-away={toggle_dropdown()}
+                type="button"
+                class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                id="user-menu-button"
+                aria-expanded="false"
+                aria-haspopup="true"
+              >
+                <span class="absolute -inset-1.5"></span>
+                <span class="sr-only">Open user menu</span>
+                <img src={~p"/images/meta_mask.svg"} alt="MetaMask Logo" class="h-8 w-8 rounded-full" />
+              </button>
+            </div>
+            <div
+              id="profile-dropdown"
+              class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="user-menu-button"
+              tabindex="-1"
+            >
+              <.link
+                navigate={~p"/user/events"}
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                role="menuitem"
+                tabindex="-1"
+                id="user-menu-item-0"
+              >
+                My Events
+              </.link>
+              <.link
+                navigate={~p"/user/tickets"}
+                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                role="menuitem"
+                tabindex="-1"
+                id="user-menu-item-1"
+              >
+                My Tickets
+              </.link>
+              <.link
+                navigate={~p"/user/settings"}
+                class="block px-4 py-2 text-sm text-gray-700  hover:bg-gray-100"
+                role="menuitem"
+                tabindex="-1"
+                id="user-menu-item-2"
+              >
+                Settings
+              </.link>
+              <.form for={%{}} action={~p"/logout"} method="delete" as={:user} phx-trigger-action={@logout} class="w-full">
+                <button
+                  class="block px-4 py-2 text-sm text-gray-700  hover:bg-gray-100"
+                  role="menuitem"
+                  tabindex="-1"
+                  id="user-menu-item-3"
+                >
+                  Sign out
+                </button>
+              </.form>
+            </div>
+          </div>
         </div>
       </div>
       <button
@@ -132,6 +190,18 @@ defmodule TickexWeb.Components.ConnectWalletButton do
       |> assign(logout: true, logged_in: false)
 
     {:noreply, socket}
+  end
+
+  defp toggle_dropdown do
+    JS.toggle(
+      to: "#profile-dropdown",
+      in:
+        {"transition ease-out duration-100", "transform opacity-0 scale-95",
+         "transform opacity-100 scale-100"},
+      out:
+        {"transition ease-in duration-75", "transform opacity-100 scale-100",
+         "transform opacity-0 scale-95"}
+    )
   end
 
   defp short_wallet_address(nil), do: ""
